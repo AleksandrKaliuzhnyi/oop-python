a12998648bc7ca1e77e03e6340f636445d83a8c9
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2021-10-19 11:19:49 +0300

    define constructor and improve code

--- main.py
+++ main.py
@@ -7,20 +7,21 @@
 
 
 class Item:
+    # constructor
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
     # method, not function
-    def calculate_total_price(self, x, y):
-        return x * y
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
-ite1 = Item()
-ite1.name = "Phone"
-ite1.price = 100
-ite1.quantity = 5
-# item = self, item1.price = x and item1.quantity = y
-print(ite1.calculate_total_price(ite1.price, ite1.quantity))
+# 1st line is an instance
+ite1 = Item("Phone", 100, 5)
+# 1st line is an instance
+ite2 = Item("Laptop", 1000, 3)
 
-ite2 = Item()
-ite2.name = "Laptop"
-ite2.price = 1000
-ite2.quantity = 3
-print(ite2.calculate_total_price(ite2.price, ite2.quantity))
+print(ite1.calculate_total_price())
+print(ite2.calculate_total_price())
