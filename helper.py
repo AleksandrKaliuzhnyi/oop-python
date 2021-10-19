8612e29d079121c341fd7e3de147464ae095fecf
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2021-10-19 16:50:26 +0300

    static and class methods

---
+++ helper.py
@@ -0,0 +1,22 @@
+# When to use class methods and when to use static methods?
+
+
+class Item:
+    pass
+
+    @staticmethod
+    def is_integer(num):
+        pass
+    '''
+    This should do something that has a relationship with the class,
+    but not something that must be unique per instance
+    '''
+
+    @classmethod
+    def instantiate_fom_csv(cls):
+        pass
+    '''
+    This should also do something that has a relationship with the class,
+    but usually, those are used to manipulate different structures of data to
+    instantiate objects, like we have done with CSV.
+    '''
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import csv
+
 
 class Item:
     # The pay rate after 20% discount
     pay_rate = 0.8
     all = []
+
     # constructor
     def __init__(self, name: str, price: float, quantity: int):
         # Validation for the receive arguments
@@ -24,14 +27,37 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_fom_csv(cls):
+        with open("items.csv", "r") as file:
+            reader = csv.DictReader(file)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+
+    @staticmethod
+    def is_integer(num):
+        # We will count out the float that are point zero
+        # For i.e: 5.0, 10.0
+        if isinstance(num, float):
+            # Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity}')"
 
 
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 3)
-
+# Item = cls
+Item.instantiate_fom_csv()
 print(Item.all)
+
+print(Item.is_integer(4))
